d08479f38f6e6a95a4092546e88e44b58d1f5e82
zhuzhe <1498852723@qq.com>
2022-06-29 15:22:02 +0800

    fix

--- php/en/texts.php
+++ php/en/texts.php
@@ -1,5 +1,6 @@
 <?php
 return [
     'welcome' => 'Welcome to our application',
-    'apples' => 'There is one apple|There are many apples',
+    'apples' => 'There is one apple',
+    'timeout' => 'Login information timed out',
 ];
